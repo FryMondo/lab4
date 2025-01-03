8cdfc9a77f4da3fc1daff25bdeefc3ccb26fa604
FryMondo <ptachyksergei@gmail.com>
2025-01-03 23:34:07 +0200

    class for letters

---
+++ src/Letter.java
@@ -0,0 +1,12 @@
+class Letter {
+    // Клас для літер слова
+    private char value;
+
+    public Letter(char value) {
+        this.value = value;
+    }
+
+    public char getValue() {
+        return value;
+    }
+}
